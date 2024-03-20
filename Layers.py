65b08f27ff6c6daae51daaf7c8d253ec304b47c1
Feltlin <82766584+Feltlin@users.noreply.github.com>
2024-03-20 00:08:39 -0400

    Change Output_Layer to inherit from Hidden_Layer.

--- Layers.py
+++ Layers.py
@@ -27,41 +27,30 @@ class Hidden_Layer:
         self.db_temp += self.db
 
     #Adjust the weight and bias to learn.
-    def learn(self,rate,batch_size):
+    def learn(self,rate:float,batch_size:int):
         self.w -= rate * self.dw_temp / (np.max(np.abs(self.dw_temp)) * batch_size)
         self.b -= rate * self.db_temp / (np.max(np.abs(self.db_temp)) * batch_size)
         self.dw_temp.fill(0)
         self.db_temp.fill(0)
 
 #Output Layer
-class Output_Layer:
+class Output_Layer(Hidden_Layer):
 
-    #Initialize function
     def __init__(self,n_input:int,n_output:int):
-        self.w = np.random.randn(n_input,n_output) * 1e-2
-        self.dw_temp = np.zeros((n_input,n_output))
-
-        self.b = np.zeros((1, n_output))
-        self.db_temp = np.zeros((1, n_output))
+        #Inherit from Hidden_Layer.
+        super().__init__(n_input,n_output)
     
-    #Output for the final prediction.
     def forward(self,input):
         self.input = input
         self.z = (input @ self.w) + self.b
+        #Change activation function to softmax.
         self.output = softmax(self.z)
 
-    #Back propagation of the output layer.
     def backward(self,desire_output):
+        #Change ∂l/∂z.
         self.dz = self.output - desire_output
         self.dw = (self.input.T @ self.dz) / 784
         self.db = self.dz / 784
         self.dinput = self.dz @ self.w.T
         self.dw_temp += self.dw
         self.db_temp += self.db
-    
-    #Adjust the weight and bias to learn.
-    def learn(self,rate:float,batch_size:int):
-        self.w -= rate * self.dw_temp / (np.max(np.abs(self.dw_temp)) * batch_size)
-        self.b -= rate * self.db_temp / (np.max(np.abs(self.db_temp)) * batch_size)
-        self.dw_temp.fill(0)
-        self.db_temp.fill(0)
